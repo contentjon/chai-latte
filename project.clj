8b7fb0bfcc71dd4fd852b6209fbbdad9ff3bad0e
Jan Krueger <jan.krueger@gateprotect.com>
2013-04-26 17:42:11 +0200

    Initial code and test cases

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject chai-latte "0.1.0-SNAPSHOT"
+  :description      "A ClojureScript port of the chai assertion library"
+  :min-lein-version "2.0.0"
+  :plugins          [[lein-cljsbuild "0.3.0"]]
+  :dependencies     [[org.clojure/clojure "1.4.0"]
+                     [mocha-latte         "0.1.0-SNAPSHOT"]]
+  :cljsbuild
+  {:builds
+   [{:source-paths ["src" "test"],
+     :id "unit",
+     :compiler
+     {:pretty-print  true,
+      :output-to     "test/unit_spec.js",
+      :optimizations :simple}}]})
