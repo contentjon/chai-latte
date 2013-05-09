1f6fe9e7f14a5ccc2f01137ddaaafea7414acd68
Jan Krüger <jan.krueger@gmail.com>
2013-05-09 12:22:41 +0200

    Added license information and project url

--- project.clj
+++ project.clj
@@ -1,5 +1,11 @@
-(defproject chai-latte "0.1.0"
+(defproject chai-latte "0.1.1"
   :description      "A ClojureScript port of the chai assertion library"
+  :url              "https://github.com/contentjon/chai-latte"
+  :license          {:name         "MIT"
+                     :url          "http://opensource.org/licenses/MIT"
+                     :distribution :repo
+                     :comments     "Same license as Chai"}
+
   :min-lein-version "2.0.0"
   :plugins          [[lein-cljsbuild "0.3.0"]]
   :dependencies     [[org.clojure/clojure "1.4.0"]
